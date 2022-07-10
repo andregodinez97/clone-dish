bb8e39d679dba512570ab96e9fda290ad01ba6e7
andregodinez97 <60364727+andregodinez97@users.noreply.github.com>
2022-07-10 12:36:59 +0100

    fix format and add scroll

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,18 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {filter, map, Observable} from "rxjs";
 import {RestaurantDataService} from "./restaurant-data.service";
 import {MenuItem, RestaurantMain} from "./restaurant-main.model";
-import { faBagShopping, faAdd } from '@fortawesome/free-solid-svg-icons';
+import {faAdd, faBagShopping} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
   styleUrls: ['./restaurant.component.scss']
 })
-export class RestaurantComponent implements OnInit {
+export class RestaurantComponent implements OnInit, AfterViewInit {
   faBagShopping = faBagShopping;
   faAdd = faAdd;
+
+  @ViewChildren('itemCategory') itemCategories!: QueryList<ElementRef>;
 
   public restaurantMain$: Observable<RestaurantMain | undefined> | undefined;
 
@@ -27,11 +29,25 @@ export class RestaurantComponent implements OnInit {
     this.restaurantMain$ = this.restaurantDataService.restaurantData$;
   }
 
+  ngAfterViewInit(): void {
+  }
+
+
   getMenuItemsByCategory(category: string): Observable<MenuItem[] | undefined> {
     return this.restaurantDataService.restaurantData$.pipe(
       filter(restaurant => !!restaurant?.menuItemList),
       map(restaurant => restaurant?.menuItemList.filter(menuItem => menuItem.itemCategory === category)),
     );
   }
+
+  scrollToView(categoryString: string): void {
+    const itemCategory = this.itemCategories.find(category => category.nativeElement.id === categoryString);
+    itemCategory?.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
+  }
+
 
 }
